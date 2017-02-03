b18feb7a24baf71329fb55acf4f7e81af84735ec
maiquel <maiquel_craash@hotmail.com>
2017-02-03 16:03:15 -0200

    New import mode - Step 5 of guide (https://github.com/verekia/js-stack-from-scratch/tree/master/tutorial/5-es6-modules-syntax)

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@
  * Created by maiquel on 03/02/17.
  */
 
-(function () {
-	"use strict";
+const Color = require('color');									//require the color library (added by yarn). https://github.com/qix-/color
+const str = "ES6";
 
-	const Color = require('color');									//require the color library (added by yarn). https://github.com/qix-/color
-	const str = "ES6";
-	const Dog = require('./dog');									//import the Dog class
+const Dog = require('./dog');									//import the Dog class (old mode)
+import NewDog from './newDog';									//import the newDog class (ES6 mode)
 
 
-	console.log("Hello World!");
-	console.log(`Hello $(str)`);
+console.log("Hello World!");
+console.log(`Hello $(str)`);
 
-	const redHexa = Color({r: 255, g:0, b:0}).hex();
-	console.log(redHexa);
+const redHexa = Color({r: 255, g: 0, b: 0}).hex();
+console.log(redHexa);
 
-	const toby = new Dog("Toby");
-	console.log(toby.bark());
+const toby = new Dog("Toby");
+console.log(toby.bark());
 
-}());
+const toto = new NewDog("Totó");
+console.log(toto.bark());
---
+++ src/newDog.js
@@ -0,0 +1,14 @@
+/**
+ * Created by maiquel on 03/02/17.
+ */
+
+
+export default class {
+	constructor(name) {
+		this.name = name;
+	}
+
+	bark() {
+		return `Uau Auuu, I am ${this.name}`;
+	}
+}
