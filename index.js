4fba092ff6146e1f3f69fd75062b3a33db70685c
maiquel <maiquel_craash@hotmail.com>
2017-02-03 10:44:18 -0200

    Added a Package - Step 2 of guide (URL: https://github.com/verekia/js-stack-from-scratch/tree/master/tutorial/2-packages)

--- index.js
+++ index.js
@@ -5,6 +5,12 @@
 (function () {
 	"use strict";
 
+	const Color = require('color');									//require the color library (added by yarn). https://github.com/qix-/color
+
+
 	console.log("Hello World!");
+
+	const redHexa = Color({r: 255, g:0, b:0}).hex();
+	console.log(redHexa);
 
 }());
