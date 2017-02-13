6eb1e988806ce81f0f45868fab7264ee1a38535d
maiquel <maiquel_craash@hotmail.com>
2017-02-13 16:38:49 -0200

    Added eslint - Step 6 of guide (https://github.com/verekia/js-stack-from-scratch/tree/master/tutorial/6-eslint)

--- gulpfile.js
+++ gulpfile.js
@@ -9,20 +9,33 @@
 	const babel = require('gulp-babel');
 	const del = require('del');
 	const exec = require('child_process').exec;
+	const eslint = require('gulp-eslint');
 
 	/* Regists all paths */
 	const paths = {
 		allSrcJs: "src/**/*.js",
+		gulpfile: 'gulpfile.js',
 		libDir: "lib"
 	};
+
+	/* Check and formats the source files */
+	gulp.task('lint', () => {
+		return gulp.src([
+			paths.allSrcJs,
+			paths.gulpfile
+		])
+			.pipe(eslint())
+			.pipe(eslint.format())
+			.pipe(eslint.failAfterError());
+	});
 
 	/* Cleans the lib directory */
 	gulp.task('clean', () => {
 		return del(paths.libDir);
 	});
 
-	/* Calls clean task, then convert to ES5 whit babel, then move to the lib directory */
-	gulp.task('build', ['clean'], () => {
+	/* Calls lint task, then clean task, then convert to ES5 whit babel, then move to the lib directory */
+	gulp.task('build', ['clean'], () => {    //add "lint" if you want to use de eslinter
 		return gulp.src(paths.allSrcJs)
 			.pipe(babel())
 			.pipe(gulp.dest(paths.libDir));
--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 Step-by-step on link:
 https://github.com/verekia/js-stack-from-scratch
+
+Build command:
+$ yarn start
